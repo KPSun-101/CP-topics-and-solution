a6120027910bc8836c860b6728d2dd05ca713e5c
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-05-25 21:15:21 +0600

    Create Raising Bacteria 579A Codeforces.cpp

---
+++ Bitwise/Raising Bacteria 579A Codeforces.cpp
@@ -0,0 +1,14 @@
+//__sun__
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+#define faster {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+int main()
+{
+    faster
+    int n;
+    cin>>n;
+    cout<<__builtin_popcount(n)<<nl;
+    return 0;
+}
