b311276f12b6e046d45fcf36de0dc1b7d40a26f9
Md Kamal Parvez Sun <kamal.prstucse1.39@gmail.com>
2026-05-25 21:08:16 +0600

    Create Specialty String 2200C Codeforces.cpp

---
+++ Stack/Specialty String 2200C Codeforces.cpp
@@ -0,0 +1,41 @@
+//__sun__
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+#define faster {ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);}
+int main()
+{
+    faster
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        stack<char>S;
+        for(int i=0;i<n;i++)
+        {
+            char c=s[i];
+            if(!S.empty()&&S.top()==c)
+            {
+                S.pop();
+            }
+            else
+            {
+                S.push(c);
+            }
+        }
+        if(S.empty())
+        {
+            cout<<"YES"<<nl;
+        }
+        else
+        {
+            cout<<"NO"<<nl;
+        }
+    }
+    return 0;
+}
